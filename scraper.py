d635f9ae13693c0ccc5ea749dc4863cd9b8d8b66
OndrejPech <pech.ondrej@gmail.com>
2021-11-10 11:59:33 +0100

    reformat code into functions

--- scraper.py
+++ scraper.py
@@ -4,50 +4,80 @@ import argparse
 from bs4 import BeautifulSoup
 import utils
 
+
+def get_league_id(args) -> str:
+    """"
+    Check if league_id is parsed in program arguments by user.
+    If not ask user for league_id and return it
+    """
+    if args.league:
+        id_league = args.league
+    else:
+        id_league = utils.user_input_num_league()
+    return id_league
+
+
+def basic_connection(id_league):
+    """
+    Check if league_id is valid. If not program quits.
+    Return bs4.element.Tag
+    """
+    league_link = MAIN_LINK + PATH + id_league
+    r = utils.check_response(league_link)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    league_tag = soup.header.find('div', {
+        'class': 'secondary-menu secondary-menu-soutez'})
+    if not league_tag:
+        print(f"League with id {id_league} probably doesn't exists. ", end='')
+        print("Please make sure you insert a valid league id.")
+        quit()
+
+    return league_tag
+
+
+def scrape_base_info(tag) -> dict:
+    """Scrape basic info from bs4.element.Tag"""
+    tag_league_info = tag.find('a', string="Tabulka")
+    link_league_info = MAIN_LINK + tag_league_info['href']
+    return utils.scrap_basic_info_league(link_league_info)
+
+
+def scrape_all_games(tag) -> dict:
+    """Scrape all the games in the league from bs4.element.Tag"""
+    tag_all_games = tag.find('a', string="Rozlosování")
+    link_games = MAIN_LINK + tag_all_games['href']
+    return utils.scrap_games_in_league(link_games)
+
+
+def create_jsons(id_league: str, base_info: dict, all_games: dict) -> None:
+    """make/ overwrite two JSON files in folder named by league_id"""
+    if not os.path.exists(id_league):
+        os.makedirs(id_league)
+
+    with open(id_league + '/basic.json', 'w') as file:
+        json.dump(base_info, file, indent=4)
+
+    with open(id_league + '/games.json', 'w') as file:
+        json.dump(all_games, file, indent=4)
+
+
 MAIN_LINK = "https://fotbalunas.cz"
-path = "/soutez/"
+PATH = "/soutez/"
 
-parser = argparse.ArgumentParser(description="""This program creates folder with \
-JSON files with all past games of the league. If  you insert league number \
-based on league id on 'https://fotbalunas.cz' \
-the program will download all info about the league and played games.\
-If you don't parse the league number you have to provide valid http link \
-inside the program.""")
-parser.add_argument("-l", "--league")
-args = parser.parse_args()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="""
+    This program creates folder \
+    with JSON files with all past games of the league. If you insert league \
+    number based on league id on 'https://fotbalunas.cz' , the program will\
+    download all info about the league and played games.\
+    If you don't parse the league number you have to provide valid http link \
+    inside the program.""")
+    parser.add_argument("-l", "--league")
+    arguments = parser.parse_args()
 
-if args.league:
-    league_id = args.league
-else:
-    league_id = utils.user_input_num_league()
+    league_id = get_league_id(arguments)
+    league_info_tag = basic_connection(league_id)
+    basic_info = scrape_base_info(league_info_tag)
+    games = scrape_all_games(league_info_tag)
 
-# BASIC CONNECTION
-league_link = MAIN_LINK + path + league_id
-r = utils.check_response(league_link)
-soup = BeautifulSoup(r.content, 'html.parser')
-tag_league_navigation = soup.header.find('div',
-                                         {'class': 'secondary-menu secondary-menu-soutez'})
-if not tag_league_navigation:
-    print(f"League with id {league_id} probably doesn't exists. ", end='')
-    print("Please make sure you insert a valid league id.")
-    quit()
-
-# SCRAPE BASIC DATA OF THE LEAGUE AND CREATE DICTIONARY
-tag_league_info = tag_league_navigation.find('a', string="Tabulka")
-link_league_info = MAIN_LINK + tag_league_info['href']
-basic_info = utils.scrap_basic_info_league(link_league_info)
-
-# SCRAPE ALL THE GAMES IN THE LEAGUE
-tag_all_games = tag_league_navigation.find('a', string="Rozlosování")
-link_games = MAIN_LINK + tag_all_games['href']
-games = utils.scrap_games_in_league(link_games)
-
-# dump to json
-if not os.path.exists(league_id):
-    os.makedirs(league_id)
-
-with open(league_id + '/basic.json', 'w') as file:
-    json.dump(basic_info, file, indent=4)
-
-with open(league_id + '/games.json', 'w') as file:
-    json.dump(games, file, indent=4)
+    create_jsons(league_id, basic_info, games)
